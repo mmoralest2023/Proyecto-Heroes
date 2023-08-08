c9fe3fa835b68f2a13996df8805b8972583eefe2
Mauricio Morales T <m.morales9@uniandes.edu.co>
2023-08-08 17:21:33 -0500

    proyecto tour of heroes

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent {
+  hero?:Hero;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {} 
+   
+  ngOnInit() {
+    this.getHero()
+    
+  }
+  getHero():void {
+    const id:number = Number (this.route.snapshot.paramMap.get('id'))
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero)
+
+  }
+  goBack():void {
+    this.location.back()
+
+  }
+  
+}
